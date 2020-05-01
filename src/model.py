916ca6f4415e599a61bbe4de1c92eb1142556661
AmanJudeMilo <amanjudemilo@berkeley.edu>
2020-05-01 19:44:30 +0000

    Added most up-to-date model files

--- src/model.py
+++ src/model.py
@@ -1,11 +1,25 @@
 import torch
 import torch.nn as nn
 
-def Flatten():
-    def forward(self, x):
-        return torch.flatten(x, star_dim=1)
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
 
-def stn(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+    
+class GlobalAvgPool(nn.Module):
+    def __init__(self):
+        super(GlobalAvgPool, self).__init__()
+        self.layer = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            Flatten()
+        )
+    
+    def forward(self, x):
+        return self.layer(x)
+
+class STN(nn.Module):
     
     def __init__(self, num_agfs):
         self.num_agfs = num_agfs
@@ -50,8 +64,7 @@ def stn(nn.Module):
             nn.ELU()
         )
         self.global_avg_pool = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1,1)),
-            Flatten(),
+            GlobalAvgPool(),
             nn.BatchNorm1d(num_features=256)
         )
         self.densefc = nn.Sequential(
@@ -61,7 +74,7 @@ def stn(nn.Module):
             nn.Dropout(p=0.5)
         )
         self.output_layer = nn.Sequential(
-            nn.Linear(in_features=256, out_features=self.num_afgs)
+            nn.Linear(in_features=256, out_features=self.num_agfs)
         )
         
         
@@ -86,8 +99,9 @@ def stn(nn.Module):
         return out
         
         
-    '''
-    def forward_intermediate(self, x):
+    def forward_all(self, x):
+        #GlobalAvgPool for intermediate layers
+        gap = GlobalAvgPool()
         
         #Convolution
         out1 = self.layer1(x)
@@ -98,13 +112,43 @@ def stn(nn.Module):
         out6 = self.layer6(out5)
         out7 = self.layer7(out6)
         
-        #Global Average Pooling
-        out_pool = self.global_avg_pool(out7)
+#         #Global Average Pooling
+#         out_pool = self.global_avg_pool(out7)
         
-        #Dense Layer
-        out = self.densefc(out_pool)
-        out = self.output_layer(out)
+#         #Dense Layer
+#         out = self.densefc(out_pool)
+#         out = self.output_layer(out)
         
-        return out1, out2, out3, out4, out5, out6, out7, out_pool, out 
-    '''
+        return [gap(out1), gap(out2), gap(out3), gap(out4), gap(out5), gap(out6), gap(out7)]
+    
+    def forward_intermediate(self, x, layer_no):
+        assert 1 <= layer_no <= 7 and isinstance(layer_no, int)
+        
+        #layers
+        layers = [self.layer1, self.layer2, self.layer3, self.layer4, self.layer5, self.layer6, self.layer7]
+        
+        #GlobalAvgPool for intermediate layers
+        gap = GlobalAvgPool()
+        
+        for i in range(layer_no - 1):
+            x = layers[i](x)
+        
+        return gap(x)
+    
+class MLP(nn.Module):
+    def __init__(self, input_dims):
+        super().__init__()
+        self.input_dims = input_dims
+        
+        self.fc = nn.Sequential(
+                    
+                    nn.Linear(self.input_dims, out_features=1024),
+                    nn.ELU(),
+                    nn.Dropout(p=0.5),
+                    nn.Linear(in_features=1024, out_features=num_targets)
+        
+        )
+        
+    def forward(self, x):
+        return self.fc(x)
         
